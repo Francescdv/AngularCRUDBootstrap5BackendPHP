4663bc8ab94cfd29984fd93700e73e45fcea3ef0
francescdv <francescdv@gmail.com>
2021-10-06 09:56:58 +0200

    no message

---
+++ angularcrud/src/app/crud.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+  userData:any;
+  userid:any;
+  singleuserdata:any
+
+  constructor(private http:HttpClient) { }
+  //get all users  details
+  public getusers()
+      {
+
+          return this.http.get('http://localhost/users.php');
+      }
+  //add new user
+  public adduser(userData)
+  {
+    return this.http.post('http://localhost/users.php/'
+  , userData).subscribe((res: Response) => {
+    this.getusers();
+  });
+  }
+
+   //delete user
+   public deleteuser(userid)
+   {
+     return this.http.post('http://localhost/users.php/'
+     , userid).subscribe((res: Response) => {});
+   }
+
+    //get single user
+  public getsingleuser(userid)
+  {
+    return this.http.post('http://localhost/users.php/'
+    , userid).subscribe((res: Response) => {
+      this.singleuserdata = res[0];
+
+
+    });
+  }
+
+  //update user
+  public updateuser(userid)
+  {
+    return this.http.post('http://localhost/users.php/'
+    , userid).subscribe((res: Response) => {});
+  }
+
+}
